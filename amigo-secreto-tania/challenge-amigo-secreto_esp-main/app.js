5168d3db19a7e0842ec459ad0cda6e3b06f6fbe3
tania <tani.oabarca@gmail.com>
2025-08-12 21:10:42 -0500

    crea el array listaAmigos

--- amigo-secreto-tania/challenge-amigo-secreto_esp-main/app.js
+++ amigo-secreto-tania/challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos=[];
+function agregarAmigo(){
+    
+}// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
