7568fed070f40365e27514dd1efe318ad3bfe0b3
tania <tani.oabarca@gmail.com>
2025-08-13 01:16:31 -0500

    crea la funcion agregar nombres

--- amigo-secreto-tania/challenge-amigo-secreto_esp-main/app.js
+++ amigo-secreto-tania/challenge-amigo-secreto_esp-main/app.js
@@ -1,4 +1,23 @@
+
 let listaAmigos=[];
+
 function agregarAmigo(){
+
+ var nombreAmigo = document.getElementById("amigo").value;
+ if(nombreAmigo!==""){
+    listaAmigos.push(nombreAmigo);   
+    limpiarCaja();
+    //alert(listaAmigos);
+    console.log(listaAmigos);
+    return listaAmigos;
+ }else{
+    alert('ingrese un nombre valido');
+ };
+  
     
-}// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+};
+
+function limpiarCaja(){
+    return document.querySelector('#amigo').value='';
+};
+
