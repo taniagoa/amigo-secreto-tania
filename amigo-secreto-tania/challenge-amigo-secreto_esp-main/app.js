4ab903f6079cc3f7eceade034ab9149d14d91594
tania <tani.oabarca@gmail.com>
2025-08-15 21:37:59 -0500

    crear la funcion lista de amigos

--- amigo-secreto-tania/challenge-amigo-secreto_esp-main/app.js
+++ amigo-secreto-tania/challenge-amigo-secreto_esp-main/app.js
@@ -1,23 +1,45 @@
 
-let listaAmigos=[];
+let arrayAmigos=[];
 
 function agregarAmigo(){
 
  var nombreAmigo = document.getElementById("amigo").value;
  if(nombreAmigo!==""){
-    listaAmigos.push(nombreAmigo);   
+    arrayAmigos.push(nombreAmigo); 
+      
     limpiarCaja();
+    crearLista();
     //alert(listaAmigos);
-    console.log(listaAmigos);
-    return listaAmigos;
+    //console.log(arrayAmigos);
+    return;
+ 
  }else{
     alert('ingrese un nombre valido');
  };
-  
+ 
     
 };
-
 function limpiarCaja(){
     return document.querySelector('#amigo').value='';
 };
+function asignarElemento(elemento,texto){
+    let elementoHTML= document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+    return;
+};
 
+function crearLista(){
+document.querySelector('#listaAmigos').innerHTML='';
+console.log(arrayAmigos.length)
+
+for (var contador=0; contador<arrayAmigos.length; contador++) {
+   let li = document.createElement('li');
+
+   li.textContent=arrayAmigos[contador]
+   document.querySelector('#listaAmigos').appendChild(li);
+  console.log(arrayAmigos[contador]);
+
+};
+};
+
+function sortearAmigo(){};
